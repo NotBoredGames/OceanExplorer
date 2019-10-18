07b5cdda6ad6bb70686dde77631645c7e86f1443
Ed Marrs <Edward.Marrs1@marist.edu>
2019-10-18 12:49:42 -0400

    Update 10/18
    -Added Bomb Mechanic
       * Still encountering a glitch where the bomb appears behind the user.

---
+++ Assets/Scripts/BombHit.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BombHit : MonoBehaviour
+{
+	
+	
+	
+	public float attackRange;
+	public LayerMask whatIsEnemies;
+	public int damage=3;
+
+
+    void Start()
+    {
+        // need to set audio source to use singular audio clip
+        
+    }
+
+    // Start is called before the first frame update
+    void Update(){
+		
+	}
+	private void OnTriggerEnter2D(Collider2D c)
+    {
+		
+		if(c.GetComponent<Collider2D>().tag == "Enemy")
+         {
+		   
+			   Debug.Log("Exploding...");
+               
+               Collider2D[] enemiesToDamage=Physics2D.OverlapCircleAll(gameObject.transform.position,attackRange,whatIsEnemies);
+			  for(int i = 0; i<enemiesToDamage.Length;i++)
+			  {
+				  //enemiesToDamage[i].GetComponent<GenericEnemyScript>().TakeDamage(damage);
+			  }
+			  Destroy(gameObject);
+		}
+		   
+		   
+	   
+	}
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color=Color.red;
+		Gizmos.DrawWireSphere(gameObject.transform.position,attackRange);
+	}
+}
---
+++ Assets/Scripts/bombLaunch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bombLaunch : MonoBehaviour
+{	
+	 public GameObject Bomb;
+    
+    
+	void Update(){
+		
+		
+		if(Input.GetKeyDown(KeyCode.X)){
+			LaunchBomb();
+			
+		}
+	}
+	
+	void LaunchBomb(){
+		Debug.Log("key is pressed.");
+		Instantiate(Bomb,new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
+	}
+}
