977514c7367142e6a8ea66189aaea6122927b2fa
Ed Marrs <Edward.Marrs1@marist.edu>
2019-10-19 15:21:31 -0400

    Update 10/19-Booom
    -Added Bomb (Right Click to Launch)
       *Bomb Currently Appears Behind Background for some reason.

---
+++ Assets/Scripts/BombLaunch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombLaunch : MonoBehaviour
+{
+    public GameObject bomb;
+    public int bombCount;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1) && bombCount>0)
+        {
+            Instantiate(bomb, new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
+            bombCount -= 1;
+        }
+    }
+}
