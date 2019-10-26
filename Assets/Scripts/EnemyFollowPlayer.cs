f15c35700f460e3eae3e22e6451fda3056356107
John-Monzo <John.Monzo1@Marist.edu>
2019-10-26 14:40:53 -0400

    Update 10/26-Follow Ai
    -Added Follow-the-player AI
        * Bullets can collide now with enemies in the Enemy tag  NOT the enemy Layer.
    Glitch(?): Everything needs to be in the UI layer to be visble on the screen.

---
+++ Assets/Scripts/EnemyFollowPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollowPlayer : MonoBehaviour
+{
+    public Transform target;//set target from inspector instead of looking in Update
+    public float speed = 10f;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+
+
+        //rotate to look at the player
+        transform.LookAt(target.position);
+        transform.Rotate(new Vector3(0, -90, 0), Space.Self);//correcting the original rotation
+
+
+        //move towards the player
+        if (Vector3.Distance(transform.position, target.position) > 1f)
+        {//move if distance from target is greater than 1
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+
+    }
+
+}
