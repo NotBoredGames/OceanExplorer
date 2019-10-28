e385bcdbec5ea79c4f7809bf2a154ca22d08acb0
IanClayman <Ian.clayman1@gmail.com>
2019-10-28 09:11:01 -0400

    Script Changes
    KillEnemyOnHit -> TurretBuleltCollision
    - Now handles collision w/ environment as well
    EnableSpotlight
    - turns on or off ALL GameObjects related to spotlight based on bool

---
+++ Assets/Scripts/EnableSpotlightScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableSpotlightScript : MonoBehaviour
+{
+
+    [SerializeField]
+    bool enableSpotlight = true;
+
+    [SerializeField]
+    GameObject[] relevantObjects = new GameObject[] { };
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject obj in relevantObjects)
+        {
+            obj.SetActive(enableSpotlight);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TurretBulletCollisionScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretBulletCollisionScript : MonoBehaviour
+{
+  
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //Debug.Log("Hit something");
+        if (col.transform.tag == "Enemy")
+        {
+            //Debug.Log("Hit Enemy.");
+            Destroy(this.gameObject);
+            Destroy(col.gameObject);
+        }
+        else if (col.transform.tag == "Environment")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    void OnBecameInvisible()
+    {
+        //Debug.Log("Object offscreen, destroying...");
+        Destroy(gameObject);
+    }
+}
