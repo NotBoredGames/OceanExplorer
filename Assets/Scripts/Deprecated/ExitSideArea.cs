92e46dc9a82740294af4eff217a3111ef3d69200
LiamMarist <31492063+LiamMarist@users.noreply.github.com>
2019-11-18 17:45:50 -0500

    Level 1
    Added incomplete version of the side areas
    level obstacles and enemies in correct places
    need to fix squid AI
    need new eel sprite
    need to add more middle ground objects

---
+++ Assets/Scripts/Deprecated/ExitSideArea.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitSideArea : MonoBehaviour
+{
+
+    public GameObject UI;
+
+
+    public void ReturnToLevel()
+    {
+        //UI.SetActive(true); // turn on UI when exiting Side Area
+    //    NEW_InheritScrollScript.scrollRate = 2;
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Invisible.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invisible : MonoBehaviour
+{
+
+    public static bool isInvisible;
+
+    void Update()
+    {
+        if (isInvisible == true)
+        {
+            // this is the invisible layer, camera does not render this
+            gameObject.layer = 22;
+        }
+
+        else
+        {
+            // returns to UI layer
+            gameObject.layer = 5;
+        }
+    }
+}
