5a5f0d981b097745eb54877682a4b13b1fb71bf0
Ed Marrs <Edward.Marrs1@marist.edu>
2019-11-24 19:01:58 -0500

    Update 11/24
    -Added Engineer and Navigator
    -Player can scroll through sub via hovering over mouse on arrow

---
+++ Assets/Scripts/Subhub/CameraMovement/CameraMoveRight.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoveRight : MonoBehaviour
+{
+    private bool isTouchingBarrier=false;
+
+    // Update is called once per frame
+    void OnMouseOver()
+    {
+        if (isTouchingBarrier == false)
+        {
+            Camera.main.transform.Translate(new Vector3(5 * Time.deltaTime, 0, 0));
+        }
+        else
+        {
+            //invisible
+        }
+
+    }
+    void OnCollisionEnter(Collision target)
+    {
+        Debug.Log("Test 3");
+        if (target.gameObject.tag ==  "SubCamBarrier")
+        {
+            Debug.Log("Test 1");
+            isTouchingBarrier = true;
+        }
+    }
+    void OnCollisionExit(Collision target)
+    {
+        if (target.gameObject.tag == "SubCamBarrier")
+        {
+            Debug.Log("Test 2");
+            isTouchingBarrier = false;
+        }
+    }
+}
