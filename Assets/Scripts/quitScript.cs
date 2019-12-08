8b01937e8ccb8e740a37925dce7a8585c2a3d09c
Kevin-Costello <kevin.costello1@marist.edu>
2019-12-07 22:56:29 -0500

    Music& Volume Inegrated
    Music with volume slider added

---
+++ Assets/Scripts/quitScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/**** Takes care of Scene Management in the Main Menu ****/
+public class quitScript : MonoBehaviour
+{
+    /*** Quits game upon hitting "Quit" button ***/
+    public void quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
