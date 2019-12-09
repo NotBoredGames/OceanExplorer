f070ec78fd775d4b737ffe26baa4e9753ad0d054
Ed Marrs <Edward.Marrs1@marist.edu>
2019-12-08 19:02:46 -0500

    Update 12/8-Functionality
    -Subhub upgrade system fully implemented
    -Each upgrade has 3 levels and varying stat changes associated
    -All data related to these upgrades (numbers) is stored in the  "PlayerStatistics.cs" Script
    -Player cannot click characters underneath the dialog UI elements (they will still hightlight)
    -Final implementations required:
        Loading Data from the main game scene
        Captain to save data and move to next level
    -Doggo is just happy to be here

--- Assets/Scripts/Subhub/DialogTrigger.cs
+++ Assets/Scripts/Subhub/DialogTrigger.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class DialogTrigger : MonoBehaviour
 {
     public Dialog dialog;
+	public string upgradeName;
+	public int upgradeLevel;
+	
 
     public void TriggerDialog()
     {
         Debug.Log("test");
         FindObjectOfType<DialogManager>().StartDialog(dialog);
+		GameObject.Find("U1Name").GetComponent<UnityEngine.UI.Text>().text = upgradeName;
     }
+
 }
--- Assets/Scripts/Subhub/OnHover.cs
+++ Assets/Scripts/Subhub/OnHover.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class OnHover : MonoBehaviour
 {
@@ -11,9 +13,10 @@ public class OnHover : MonoBehaviour
     void OnMouseEnter()
     {
         
-        startcolor = GetComponent<Renderer>().material.color;
-        GetComponent<Renderer>().material.color = Color.yellow;
-        textToShow.GetComponent<MeshRenderer>().enabled = true;
+			startcolor = GetComponent<Renderer>().material.color;
+			GetComponent<Renderer>().material.color = Color.yellow;
+			textToShow.GetComponent<MeshRenderer>().enabled = true;
+			 
       
         
             
@@ -22,12 +25,104 @@ public class OnHover : MonoBehaviour
     }
     void OnMouseExit()
     {
-        textToShow.GetComponent<MeshRenderer>().enabled = false;
-        GetComponent<Renderer>().material.color = startcolor;
+		
+			textToShow.GetComponent<MeshRenderer>().enabled = false;
+			GetComponent<Renderer>().material.color = startcolor;
+		
     }
     void OnMouseDown()
     {
+		if (!EventSystem.current.IsPointerOverGameObject())
+        {
         
         dialogToPlay.TriggerDialog();
+		
+		if(gameObject.name=="Engineer"){
+			
+			int currUpgradeLevel = GameObject.Find("PlayerUpgradeController").GetComponent<playerStatistics>().engineerLevel;
+			GameObject.Find("U1T1Box").GetComponent<Image>().enabled=false;
+			GameObject.Find("U1T2Box").GetComponent<Image>().enabled=false;
+			GameObject.Find("U1T3Box").GetComponent<Image>().enabled=false;
+			if(currUpgradeLevel>=1){
+				GameObject.Find("U1T1Box").GetComponent<Image>().enabled=true;
+			}
+			if(currUpgradeLevel>=2){
+				GameObject.Find("U1T2Box").GetComponent<Image>().enabled=true;
+			}
+			if(currUpgradeLevel==3){
+				GameObject.Find("U1T3Box").GetComponent<Image>().enabled=true;
+			}
+		}
+		else if(gameObject.name=="Weaponsmith"){
+			
+			int currUpgradeLevel = GameObject.Find("PlayerUpgradeController").GetComponent<playerStatistics>().weaponsmithLevel;
+			GameObject.Find("U1T1Box").GetComponent<Image>().enabled=false;
+			GameObject.Find("U1T2Box").GetComponent<Image>().enabled=false;
+			GameObject.Find("U1T3Box").GetComponent<Image>().enabled=false;
+			if(currUpgradeLevel>=1){
+				GameObject.Find("U1T1Box").GetComponent<Image>().enabled=true;
+			}
+			if(currUpgradeLevel>=2){
+				GameObject.Find("U1T2Box").GetComponent<Image>().enabled=true;
+			}
+			if(currUpgradeLevel==3){
+				GameObject.Find("U1T3Box").GetComponent<Image>().enabled=true;
+			}
+		}
+		else if(gameObject.name=="Navigator"){
+			
+			int currUpgradeLevel = GameObject.Find("PlayerUpgradeController").GetComponent<playerStatistics>().navigatorLevel;
+			GameObject.Find("U1T1Box").GetComponent<Image>().enabled=false;
+			GameObject.Find("U1T2Box").GetComponent<Image>().enabled=false;
+			GameObject.Find("U1T3Box").GetComponent<Image>().enabled=false;
+			if(currUpgradeLevel>=1){
+				GameObject.Find("U1T1Box").GetComponent<Image>().enabled=true;
+			}
+			if(currUpgradeLevel>=2){
+				GameObject.Find("U1T2Box").GetComponent<Image>().enabled=true;
+			}
+			if(currUpgradeLevel==3){
+				GameObject.Find("U1T3Box").GetComponent<Image>().enabled=true;
+			}
+		}
+		else if(gameObject.name=="Scrapper"){
+			
+			int currUpgradeLevel = GameObject.Find("PlayerUpgradeController").GetComponent<playerStatistics>().scrapperLevel;
+			GameObject.Find("U1T1Box").GetComponent<Image>().enabled=false;
+			GameObject.Find("U1T2Box").GetComponent<Image>().enabled=false;
+			GameObject.Find("U1T3Box").GetComponent<Image>().enabled=false;
+			if(currUpgradeLevel>=1){
+				GameObject.Find("U1T1Box").GetComponent<Image>().enabled=true;
+			}
+			if(currUpgradeLevel>=2){
+				GameObject.Find("U1T2Box").GetComponent<Image>().enabled=true;
+			}
+			if(currUpgradeLevel==3){
+				GameObject.Find("U1T3Box").GetComponent<Image>().enabled=true;
+			}
+		}
+		else if(gameObject.name=="Biologist"){
+			
+			int currUpgradeLevel = GameObject.Find("PlayerUpgradeController").GetComponent<playerStatistics>().marineBiologistLevel;
+			GameObject.Find("U1T1Box").GetComponent<Image>().enabled=false;
+			GameObject.Find("U1T2Box").GetComponent<Image>().enabled=false;
+			GameObject.Find("U1T3Box").GetComponent<Image>().enabled=false;
+			if(currUpgradeLevel>=1){
+				GameObject.Find("U1T1Box").GetComponent<Image>().enabled=true;
+			}
+			if(currUpgradeLevel>=2){
+				GameObject.Find("U1T2Box").GetComponent<Image>().enabled=true;
+			}
+			if(currUpgradeLevel==3){
+				GameObject.Find("U1T3Box").GetComponent<Image>().enabled=true;
+			}
+		}
+		else if(gameObject.name=="Dog"){
+			GameObject.Find("U1T1Box").GetComponent<Image>().enabled=true;
+			GameObject.Find("U1T2Box").GetComponent<Image>().enabled=true;
+			GameObject.Find("U1T3Box").GetComponent<Image>().enabled=true;
+		}
+		}
+		
     }
 }
