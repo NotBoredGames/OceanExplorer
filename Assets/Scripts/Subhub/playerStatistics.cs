a04cf3bfaee8280a0c7c9365aa50dea7d76a4191
Ed Marrs <Edward.Marrs1@marist.edu>
2019-12-10 11:57:27 -0500

    Update 12/10-Finding the NPCS
    -"Found" bools have been added in Player Stats
    -Turning the bools on and off enable and disable the NPCS

--- Assets/Scripts/Subhub/playerStatistics.cs
+++ Assets/Scripts/Subhub/playerStatistics.cs
@@ -8,41 +8,100 @@ public class playerStatistics : MonoBehaviour
 	
 	public int healthAmt;
 	public int engineerLevel;
+	public bool engineerFound;
 	
 	public int bombAmt;
 	public int weaponsmithLevel;
+	public bool weaponsmithFound;
 	
 	public int subSpeed;
 	public int navigatorLevel;
+	public bool navigatorFound;
 	
 	public int scrapIncrease;
 	public int scrapperLevel;
+	public bool scrapperFound;
 	
 	public int damageAmt;
 	public int marineBiologistLevel;
+	public bool marineBiologistFound;
+	
+	private GameObject Engineer;
+	private GameObject Weaponsmith;
+	private GameObject Navigator;
+	private GameObject Scrapper;
+	private GameObject MarineBiologist;
     // Start is called before the first frame update
     void Start()
     {
 		//load the values
        	   healthAmt=3;
 		   engineerLevel=0;
+		   engineerFound=false;
 		   
 		   bombAmt=3;
 		   weaponsmithLevel=0;
+		   weaponsmithFound=false;
 		   
 		   subSpeed=1;
 		   navigatorLevel=0;
+		   navigatorFound=false;
 		   
 		   scrapIncrease=0;
 		   scrapperLevel=0;
+		   scrapperFound=false;
 		   
 		   damageAmt=1;
 		   marineBiologistLevel=0;
+		   marineBiologistFound=false;
+		   
+		   Engineer=GameObject.Find("Engineer");
+		   Weaponsmith=GameObject.Find("Weaponsmith");
+		   Navigator=GameObject.Find("Navigator");
+		   Scrapper=GameObject.Find("Scrapper");
+		   MarineBiologist=GameObject.Find("Biologist");
+		   
+		   
+		   
+		   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(engineerFound){
+			Engineer.SetActive(true);
+		}
+		else if(!engineerFound){
+			Engineer.SetActive(false);
+		}
+		
+		if(weaponsmithFound){
+			Weaponsmith.SetActive(true);
+		}
+		else if(!weaponsmithFound){
+			Weaponsmith.SetActive(false);
+		}
+		
+		if(navigatorFound){
+			Navigator.SetActive(true);
+		}
+		else if(!navigatorFound){
+			Navigator.SetActive(false);
+		}
+		
+		if(scrapperFound){
+			Scrapper.SetActive(true);
+		}
+		else if(!scrapperFound){
+			Scrapper.SetActive(false);
+		}
+		
+		if(marineBiologistFound){
+			MarineBiologist.SetActive(true);
+		}
+		else if(!marineBiologistFound){
+			MarineBiologist.SetActive(false);
+		}
     }
 }
