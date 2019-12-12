90cb89fddd714fdc414a8b14504d36051576df35
IanClayman <Ian.clayman1@gmail.com>
2019-12-12 15:59:47 -0500

    Added player death, load into subhub on boss death

--- Assets/Scripts/HealthManagment.cs
+++ Assets/Scripts/HealthManagment.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HealthManagment : MonoBehaviour
 {
 
     [SerializeField]
     string subControllerString = "Submarine Info Controller";
+
+    [SerializeField]
+    GameObject playerDeathExplosion;
 
     SubmarineSettingsScript subSettings;
 
@@ -16,6 +20,8 @@ public class HealthManagment : MonoBehaviour
 
         if (subSettings == null)
             Debug.LogError("[[HealthManagement]] Script on GameObject " + this.gameObject.name + " unable to find SubmarineSettingsScript!");
+
+        StartCoroutine(PlayerDead());
     }
 
     // player takes damage when hit by enemy mine explosion
@@ -27,6 +33,18 @@ public class HealthManagment : MonoBehaviour
             subSettings.SetCurrentHP(subSettings.GetCurrentHP() - 1);
          
         }
+    }
+
+    IEnumerator PlayerDead()
+    {
+        while (subSettings.GetCurrentHP() > 0)
+            yield return null;
+
+        Debug.Log("DEAD");
+        GameObject explosion = Instantiate(playerDeathExplosion);
+        explosion.transform.position = transform.position;
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene("TitleScreen");
     }
  
 
