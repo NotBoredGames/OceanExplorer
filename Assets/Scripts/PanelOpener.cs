61bfa29da1db20d3b956ebe303e6bb4c8f39e6c2
Ed Marrs <Edward.Marrs1@marist.edu>
2019-12-12 18:25:11 -0500

    Revert "Merge branch 'FinalWeek-LiamO' of https://github.com/NotBoredGames/OceanExplorer into FinalWeek-LiamO"
    This reverts commit aced68c1c3ba9526074b142fed0f56e1a9fd5941, reversing
    changes made to 410e40f02693015f177b84564f49fe9c604892f6.

---
+++ Assets/Scripts/PanelOpener.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelOpener : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Panel;
+
+    public void PanelOpen()
+    {
+        if (Panel != null)
+        {
+            bool isActive = Panel.activeSelf;
+            Panel.SetActive(!isActive);
+        }
+    }
+}
